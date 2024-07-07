3bc56b998704701516f6cede275f513f7ab9b93d
Eyeri1120 <163603335+Eyeri1120@users.noreply.github.com>
2024-07-06 23:41:13 -0600

    feat: Script con las librerías a usar en el proyecto

---
+++ cod/python/librerias.py
@@ -0,0 +1,19 @@
+
+# Librerías comunes
+import pandas as pd
+import numpy as np
+import time
+import timeit
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Librerías menos comunes
+import numexpr as ne
+import numba as nb
+from numba import jit, njit, prange
+from joblib import Parallel, delayed
+from IPython.display import clear_output
+from sklearn.model_selection import train_test_split
+from scipy.stats import norm
+
+
