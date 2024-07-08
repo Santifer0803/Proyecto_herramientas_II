1ba315dd90846494da211c337d77fedea30c04f7
Eyeri1120 <163603335+Eyeri1120@users.noreply.github.com>
2024-07-08 14:12:17 -0600

    feat: Script con las clases importadas

---
+++ cod/python/clases.py
@@ -0,0 +1,9 @@
+
+# Clases
+from GenerarDataframes import GenerarDataframes
+from GenerarGraficos import GenerarGraficos
+from Madre import Madre
+from ModeloEstocastico import ModeloEstocastico
+from OperacionesBasicas import OperacionesBasicas
+from RegresionLogistica import RegresionLogistica
+from TrabajoDataframes import TrabajoDataframes
